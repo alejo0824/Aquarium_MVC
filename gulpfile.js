1e9a58b20277e9982e9c61175987330b79153e72
Alejandro Tellez <alejo082495@gmail.com>
2022-04-20 12:44:35 -0500

    Se instala varias librerías NPM para minificar el CSS en nuestro proyecto con el fin de que no puedan leer nuestros diseños en un navegador con facilidad, adicionalmente se instala otra librería de NPM gulp-sourcemaps para poder leer nosotros los estilos con el fin de modificarlos, editarlos o más adelante actualizarlos

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
-//************************************************************************* */
+//===========================================================/
 //Librerias
-//************************************************************************* */
+//===========================================================/
 const { src, dest, parallel, watch } = require('gulp'); // Gulp
-const sass = require('gulp-sass')(require('sass')); // Compilador de SASS
-//---------------------------------------------------------------------------
-//************************************************************************* */
+const sourcemaps = require('gulp-sourcemaps');
+//***************************CSS******************************
+const sass = require('gulp-sass')(require('sass')); //Compilador de SASS
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+//===========================================================/
 
 const srcFile = {
     scss: 'src/scss/**/*.scss'
@@ -14,11 +18,18 @@ const build = {
     css: 'public/build/css'
 }
 
-function css(done) {
-    src(srcFile.scss) //Identificar el archivo .SCCS a compilar
+function css() {
+    return src(srcFile.scss) //Identificar el archivo .SCCS a compilar
+        .pipe(sourcemaps.init())
         .pipe(sass()) // Compilarlo
-        .pipe(dest('public/build/css')) //Almacenarlo
-    done();
+        .pipe(postcss([autoprefixer(), cssnano()])) //Mimificar CSS
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest(build.css)) //Almacenarlo
+}
+
+function watchArchivos() {
+    watch(srcFile.scss, css)
 }
 
 exports.css = css;
+exports.watchArchivos = watchArchivos;
